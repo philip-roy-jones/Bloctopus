32c70aa0d8e3f7f7d20dade9f05d413b7b94cd1e
Philip Roy Jones <145068574+philip-roy-jones@users.noreply.github.com>
2025-05-13 15:33:00 -0400

    feat(auth): add cookie parser middleware for handling cookies in requests

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const port = 1111;
 const appUrl = process.env.APP_URL || "http://localhost:5173";
 
 app.use(cors({ origin: appUrl, credentials: true }));
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api', authRoutes);
 
