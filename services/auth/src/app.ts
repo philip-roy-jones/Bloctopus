25c5d03d73d37498b576160e0fd6f61cf1665063
Philip Roy Jones <145068574+philip-roy-jones@users.noreply.github.com>
2025-05-22 15:05:54 -0400

    feat(auth, tasks): implement internal request middleware for API access control

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import cookieParser from 'cookie-parser';
 import { WEB_URL } from './config/config';
+import { internalOnly } from './middleware/internalOnly';
 
 const app = express();
 const appUrl = WEB_URL || "http://localhost:5173";
@@ -10,6 +11,6 @@ const appUrl = WEB_URL || "http://localhost:5173";
 app.use(cors({ origin: appUrl, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', internalOnly, authRoutes);
 
 export default app;
--- services/tasks/src/app.ts
+++ services/tasks/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import taskRoutes from './routes/taskRoutes';
+import { internalOnly } from './middleware/internalOnly';
 
 const app = express();
 
 app.use(cors({ origin: "localhost:5173", credentials: true }));
 app.use(express.json());
-app.use('/api/tasks', taskRoutes);
+app.use('/api/tasks', internalOnly, taskRoutes);
 
 export default app;
