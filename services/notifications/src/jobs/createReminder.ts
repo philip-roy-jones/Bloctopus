f8b7be1c261f28458057f56514a67be120cb8cef
Philip Roy Jones <145068574+philip-roy-jones@users.noreply.github.com>
2025-06-17 17:23:20 -0400

    fix(jobs): restore dynamic delay calculation in handleCreateReminder function

--- services/notifications/src/jobs/createReminder.ts
+++ services/notifications/src/jobs/createReminder.ts
@@ -4,8 +4,7 @@ import { CreateReminderData } from "@/types"
 export async function handleCreateReminder(data: CreateReminderData) {
   await reminderQueue.add(`reminder-${data.taskId}`, data, {
     jobId: `reminder-${data.taskId}`,
-    delay: 10000,
-    // delay: new Date(data.remindAt).getTime() - Date.now(),
+    delay: new Date(data.remindAt).getTime() - Date.now(),
     removeOnComplete: true,
     removeOnFail: true,
   })
